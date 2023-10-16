f974c3ffe6e681ab00f7adb9485822b277ecd550
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-10-16 18:09:46 +0200

    hw01: Added functionality for storing times of specific trains

--- hw01/main.c
+++ hw01/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
+
+// Struktura seskupuje ruzne souvisejici datove polozky do jedne logicke jednotky
+// Kazda polozka v 'struct' ma svuj vlastni datovy typ
+// Nazev struktury je definovan na konci - v vem pripade 'TrainSchedule'
+typedef struct {
+    char arrival[6];
+    char departure[6];
+} TrainSchedule;
+
 int main(){
-        
+    // Zde vytvarim instance struktury TrainSchedule
+    // Kazda z techto instanci uklada sve vladni kopie clenu
+    TrainSchedule trains[3];
+    char* trainNames[] = {"A", "B", "C"};
+
+    // Tento for prochazi array vlaku
+    for (int i = 0; i < 3; i++){
+        // Vypisuje cas prijezdu vlaku pro specificke jmeno vlaku dle indexu v array
+        printf("Cas prijezdu vlaku %s:\n", trainNames[i]);
+        // Cte uzivatelsky input na standartnim vstupu - stdin
+        // a uklada jej do clenu 'arrival' aktualniho vlaku z pole 'trains[i]'
+        fgets(trains[i].arrival, sizeof(trains[i].arrival), stdin);
+
+        // Vypisuje cas prijezdu vlaku pro specificke jmeno vlaku dle indexu v array
+        printf("Cas odjezdu vlaku %s:\n", trainNames[i]);
+        // Cte uzivatelsky input, uklada jej do clenu 'departure' aktualniho vlaku z pole
+        fgets(trains[i].departure, sizeof(trains[i].departure), stdin);
+    }
 
 }
