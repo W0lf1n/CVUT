216d60dc251c1b826a4f2bf6a6877a4df71fe0aa
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-10-21 00:18:16 +0200

    train04: Comments added

--- train04/main.c
+++ train04/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * Function to calculate the number of digits in a given number
+ */
 int getDigits(int number) {
     int digits = 0;
     while (number) {
@@ -11,43 +14,42 @@ int getDigits(int number) {
 }
 
 /*
- *
+ * Main function to execute the program
  */
 int main(){
     int n;
     printf("Rozsah:\n");
 
+    // Check for valid input
     if (scanf("%d", &n) != 1 || n <= 0){
         printf("Nespravny vstup.\n");
         exit(1);
     }
 
-    int maxProduct = n * n;
-    int width = getDigits(maxProduct) + 1;
+    int maxProduct = n * n; // Calculate the maximum product in the table
+    int width = getDigits(maxProduct) + 1; // Determine column width based on maximum product
 
-    printf("%*c|", width - 1, ' ');  // Adjusted space before the vertical bar
+    printf("%*c|", width - 1, ' ');
     for (int i = n; i >= 1; i--) {
-        printf("%*d", width, i);
+        printf("%*d", width, i); // Print column labels
     }
     printf("\n");
 
-    // Print delimiters
     for (int i = 0; i < width - 1; i++) {
-        printf("-");
+        printf("-"); // Print left part of the delimiter before '+'
     }
     printf("+");
     for (int i = 0; i < n * width; i++) {
-        printf("-");
+        printf("-"); // Print right part of the delimiter after '+'
     }
     printf("\n");
 
-    // Print table
     for (int i = 1; i <= n; i++) {
-        printf("%*d|", width - 1, i);
+        printf("%*d|", width - 1, i); // Print the row label
         for (int j = n; j >= i; j--) {
-            printf("%*d", width, i * j);
+            printf("%*d", width, i * j); // Print product
         }
-        printf("\n");
+        printf("\n"); // New line at the end of each row
     }
 
     return 0;
