44e38defb41d460df553295ea15d96440951dffd
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-10-26 15:09:09 +0200

    hw02: Full points on Progtest

--- hw02/chinese_remainder_theorem.c
+++ hw02/chinese_remainder_theorem.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 
-/**
- * @brief Validates the input 
- *
- * @param message_count The count of messages.
- * @param char_count An array representing the count of characters in each message
- * @param pipe_count An array representing the count of '|' in each message
- * @param space_count An array representing the ocunt of whitespaces in each message
- */
-void validateInput(int message_count, int char_count[], int pipe_count[], int space_count[]) {
-    if (message_count < 2 
-        || char_count[0] == 0 || pipe_count[0] != 1 || space_count[0] > 0
-        || char_count[1] == 0 || pipe_count[1] != 1 || space_count[1] > 0) {
-        printf("Nespravny vstup.\n");
-        exit(1);
-    }
-}
 
 /**
  * @brief Prints the synchronization time or a failure message
@@ -42,15 +25,69 @@ void outputResult(int synchronization_time) {
  * @param second_message_sum The sum of message units for the second message
  * @return The GCD of the two input sums
  */
-int gcd(int a, int b) {
-    if (b == 0)
-        return a;
-    return gcd(b, a % b);
+int GCD(int first_message_sum, int second_message_sum){
+    int tmp;
+
+    if (first_message_sum < second_message_sum){
+        tmp = first_message_sum;
+        first_message_sum = second_message_sum;
+        second_message_sum = tmp;
+    }
+    while (second_message_sum > 0){
+        tmp = first_message_sum % second_message_sum;
+        first_message_sum = second_message_sum;
+        second_message_sum = tmp;
+    }
+    return first_message_sum;
 }
 
-int lcm(int a, int b) {
-    return (a * b) / gcd(a, b);
+
+// Funkce pro výpočet modulárního inverzního prvku
+int modInverse(int a, int m) {
+    int m0 = m, t, q;
+    int x0 = 0, x1 = 1;
+    while (a > 1) {
+        if (m == 0){
+            break;
+        }
+        q = a / m;
+        t = m;
+        m = a % m;
+        a = t;
+        t = x0;
+        x0 = x1 - q * x0;
+        x1 = t;
+    }
+
+    if (x1 < 0) x1 += m0;
+
+    return x1;
 }
+
+// Funkce pro nalezení minimálního x splňujícího systém rovnic x = a[i] (mod m[i])
+long long findMinX(int a[], int m[], int k) {
+    long long prod = 1;
+    for (int i = 0; i < k; i++) {
+        if (m[i] == 0) {
+            return -1;
+        }
+        prod *= m[i];
+    }
+
+    long long result = 0;
+    for (int i = 0; i < k; i++) {
+        long long pp = prod / m[i];
+        int inv = modInverse(pp, m[i]);
+        if (inv == -1) {
+            return -1;
+        }
+        result += a[i] * inv * pp;
+    }
+
+    return result % prod;
+}
+
+
 
 /**
  * @brief Calculates the synchronization time for the two messages
@@ -64,29 +101,29 @@ int lcm(int a, int b) {
  * @return The synchronization time at which the two messages are in sync.
  */
 int findSynchronizationTime(int message_sum_before[], int message_sum_after[], int message_count) {
-    int step[2] = { message_sum_before[0] + message_sum_after[0], message_sum_before[1] + message_sum_after[1] };
+    int a[2];
+    int m[2];
 
-    int gcd_result = gcd(step[0], step[1]);
-    int lcm_result = (step[0] / gcd_result) * step[1];
+    m[0] = message_sum_before[0] + message_sum_after[0];
+    m[1] = message_sum_before[1] + message_sum_after[1];
 
-    for (int t = message_sum_before[0]; t <= lcm_result; t += step[0]) {
-        if ((t - message_sum_before[1]) % step[1] == 0) {
-            return t;
-        }
+    a[0] = message_sum_before[0] % m[0];
+    a[1] = message_sum_before[1] % m[1];
+
+    // Prověření, zda je řešení možné
+    int gcd_result = GCD(m[0], m[1]);
+    if ((a[0] - a[1]) % gcd_result != 0) {
+        return -1;
     }
 
-    for (int t = message_sum_before[1]; t <= lcm_result; t += step[1]) {
-        if ((t - message_sum_before[0]) % step[0] == 0) {
-            return t;
-        }
+    int sync_time = (int)findMinX(a, m, 2);
+
+    if (sync_time % a[0] == 0) {
+        sync_time /= a[0];
     }
 
-    return -1;
+    return sync_time/gcd_result;
 }
-
-
-
-
 
 /**
  * @brief Processes the input, help validates it, and help calculates the sync. time
@@ -94,6 +131,7 @@ int findSynchronizationTime(int message_sum_before[], int message_sum_after[], i
  * This function reads the input, computes the sum of units before and after '|'
  * and handles different scenarios to output the result
  */
+
 void processAndValidateInput() {
     printf("Zpravy:\n");
 
@@ -106,9 +144,9 @@ void processAndValidateInput() {
 
     while (message_count < 2) {
         char ch = fgetc(stdin);
-        
+
         if (ch == '\n') {
-            if (char_count[message_count] == 0 || char_count[message_count] == pipe_count[message_count]) {
+            if (char_count[message_count] == 0){
                 printf("Nespravny vstup.\n");
                 exit(1);
             }
@@ -122,47 +160,48 @@ void processAndValidateInput() {
             char_count[message_count]++;
             if (ch == '|') {
                 pipe_count[message_count]++;
-                if (pipe_count[message_count] >= 2){
-                    printf("Nespravny vstup.\n");
-                        exit(1);
-                }
             } else if (ch == ' ') {
                 space_count[message_count]++;
                 printf("Nespravny vstup.\n");
                 exit(1);
             } else {
-                if (islower(ch) || ch == '|') {
-                    int pulse_length = 1 << (ch - 'a');  // Převod znaku na časovou jednotku
-                    if (pipe_count[message_count] == 0) {  // Před '|'
-                        message_sum_before[message_count] += pulse_length;
-                    } else {  // Po '|'
-                        message_sum_after[message_count] += pulse_length;
-                    }
-                } else {
-                    printf("Nespravny vstup.\n");
-                    exit(1);
+                int pulse_length = 1 << (ch - 'a');  // Převod znaku na časovou jednotku
+                if (pipe_count[message_count] == 0) {  // Před '|'
+                    message_sum_before[message_count] += pulse_length;
+                } else {  // Po '|'
+                    message_sum_after[message_count] += pulse_length;
                 }
             }
         }
     }
 
-    validateInput(message_count, char_count, pipe_count, space_count);  // Validace zpráv po načtení obou zpráv
+    if (message_count < 2
+        || char_count[0] == 0 || pipe_count[0] != 1 || space_count[0] > 0
+        || char_count[1] == 0 || pipe_count[1] != 1 || space_count[1] > 0) {
+        printf("Nespravny vstup.\n");
+        exit(1);
+    }
 
-    int gcd_result = gcd(message_sum_before[0]+message_sum_after[0], message_sum_before[1] + message_sum_after[1]);
+    int gcd_result = GCD(message_sum_before[0]+message_sum_after[0], message_sum_before[1] + message_sum_after[1]);
     if ((message_sum_before[0] - message_sum_before[1]) % gcd_result != 0){
-        outputResult(-1);
+        printf("Nelze dosahnout.\n");
         return;
     }
-    // Kontrola, zda je znak '|' na začátku nebo na konci některé zprávy
+
     for (int i = 0; i < message_count; i++) {
         if (char_count[i] == pipe_count[i] || message_sum_before[i] == 0 || message_sum_after[i] == 0) {
-            outputResult(0);
+            printf("Synchronizace za: 0\n");
             return;
         }
     }
 
     int sync_time = findSynchronizationTime(message_sum_before, message_sum_after, message_count);
-    outputResult(sync_time);
+
+    if (sync_time % message_sum_before[0] == 0) {
+        sync_time /= message_sum_before[0];
+    }
+
+    printf("Synchronizace za: %d\n", sync_time);
 }
 
 /**
@@ -175,4 +214,5 @@ int main() {
 
     return 0;
 
-}
+    }
+
