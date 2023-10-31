6665da7879e3444a3a553f67aeeec6d58f2dd510
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-10-31 16:50:06 +0100

    train06: First working version of the program

--- train06/main.c
+++ train06/main.c
@@ -3,8 +3,37 @@
 #include <assert.h>
 #endif /* __PROGTEST__ */
 
+
+int isValidDay(int year, int month) {
+    if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+        return 31;
+    } else if ((month == 2) && ((year%400==0 && year!=4000) || (year%4==0 && year%100!=0))){
+        return 29;
+    } else if (month == 2){
+        return 28;
+    } else {
+        return 30;
+    }
+}
+
+
 int dateToIndex(int day, int month, int year, int *idx) {
-    /* TODO: Your code here */
+    if (year < 2000 || (1 > month || month > 12) || ( 1 > day || day > isValidDay(year, month))) {
+
+        *idx = 0;
+
+        return 0;
+    }
+
+    int sum_day = day;
+
+    for (int i = 1; i < month; i++) {
+        sum_day += isValidDay(year, i);
+    }
+
+    *idx = sum_day;
+
+    return 1;
 }
 
 #ifndef __PROGTEST__
