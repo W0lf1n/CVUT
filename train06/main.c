44d3e60902f059c60ee286c998912bdfa98933ec
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-10-31 17:22:44 +0100

    train06: 95% on progtest

--- train06/main.c
+++ train06/main.c
@@ -10,16 +10,34 @@
  * @param month Is month in which we want to check the validation
  */
 int isValidDay(int year, int month) {
-    if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-        return 31;
-    } else if ((month == 2) && ((year%400==0 && year!=4000) || (year%4==0 && year%100!=0))){
-        return 29;
-    } else if (month == 2){
-        return 28;
+    if(year >= 4000) {
+        if(month == 2) {
+            return 28;
+        } else if(month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else {
+            return 31;
+        }
     } else {
-        return 30;
+        if(month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+            return 31;
+        } else if ((month == 2) && ((year%400==0) || (year%4==0 && year%100!=0))){
+            return 29;
+        } else if (month == 2){
+            return 28;
+        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+            return 30;
+        } else {
+            return -1;  
+        }
     }
 }
+
+
+
+
+
+
 
 /**
  * @brief Function calculates the day in the year
@@ -29,23 +47,25 @@ int isValidDay(int year, int month) {
  * @param year Given year
  */
 int dateToIndex(int day, int month, int year, int *idx) {
-    if (year < 2000 || (1 > month || month > 12) || ( 1 > day || day > isValidDay(year, month))) {
+    int maxDay = isValidDay(year, month);
 
+    if (year < 2000 || month < 1 || month > 12 || day < 1 || day > maxDay) {
         *idx = 0;
-
         return 0;
     }
 
-    int sum_day = day;
+    int sum_day = 0;
 
-    for (int i = 2; i <= month; i++) {
-        sum_day += isValidDay(year, i-1);
+    for (int i = 1; i < month; i++) {
+        sum_day += isValidDay(year, i);
     }
 
+    sum_day += day;
     *idx = sum_day;
 
     return 1;
 }
+
 
 #ifndef __PROGTEST__
 
@@ -70,7 +90,8 @@ int main (int argc, char * argv []) {
     assert(dateToIndex( 1,  8, 524000, &idx) == 1 && idx == 213);
     assert(dateToIndex(31, 10, 640000, &idx) == 1 && idx == 304);
     assert(dateToIndex(29,  2, 360000, &idx) == 0);
-
+    assert(dateToIndex(29, 2, 642400, &idx) == 1);
+    assert(dateToIndex(8, 11, 278000, &idx) == 1 && idx == 313);
     return 0;
 }
 #endif /* __PROGTEST__ */
