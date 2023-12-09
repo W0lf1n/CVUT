5ec6d4a1099fcf9bc52323943b2d4d377c276c83
Petr Bohac <bohacpe6@fit.cvut.cz>
2023-12-09 11:05:06 +0100

    train08: Saving changes

--- train08/main.c
+++ train08/main.c
@@ -1,24 +1,72 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void filledGameBoard(char **gameBoard, int rows){
+    for(int i = 0; i < rows; i++){
+        printf("%s\n", gameBoard[i]);
+    }
+}
 
 void getGameBoard(){
-    printf("Zadejte hraci plochu:\n");
-    char gameBoardPiece = ' ';
-    
+    int rows = 0;
+    char line[1024];
 
-    while(scanf(" %c", &gameBoardPiece) != EOF){
-        if (gameBoardPiece != '.' && gameBoardPiece != '*'){
-            printf("%s\n", &gameBoardPiece);
-            printf("Nespravny vstup.\n");
-            exit(0);
+    char **gameBoard = NULL;
+
+    while(fgets(line, sizeof(line), stdin)){
+        line[strcspn(line, "\n")] = 0;
+
+        // Kontrola platnosti znaků
+        for (int i = 0; line[i] != '\0'; i++) {
+            if (line[i] != '.' && line[i] != '*') {
+                fprintf(stderr, "Nespravny vstup\n");
+                // Uvolnění již alokované paměti
+                for (int j = 0; j < rows; j++) {
+                    free(gameBoard[j]);
+                }
+                free(gameBoard);
+                return;
+            }
         }
-        printf("ahoj\n");
+
+        // Realokace pole pro nový řádek
+        char **temp = realloc(gameBoard, (rows + 1) * sizeof(char*));
+        if (temp == NULL) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            for (int i = 0; i < rows; i++) {
+                free(gameBoard[i]);
+            }
+            free(gameBoard);
+            return;
+        }
+        gameBoard = temp;
+
+        // Alokace paměti pro nový řádek a kopírování obsahu
+        gameBoard[rows] = malloc(strlen(line) + 1);
+        if (gameBoard[rows] == NULL) {
+            fprintf(stderr, "Memory allocation failed.\n");
+            for (int i = 0; i < rows; i++) {
+                free(gameBoard[i]);
+            }
+            free(gameBoard);
+            return;
+        }
+        strcpy(gameBoard[rows], line);
+        rows++;
     }
+    filledGameBoard(gameBoard, rows);
+
+    // Uvolnění paměti
+    for(int i = 0; i < rows; i++){
+        free(gameBoard[i]);
+    }    free(gameBoard);
+
     printf("tak cau\n");
 }
 
 int main(){
-
+    printf("Zadejte hraci plochu:\n");
     getGameBoard();
 
     return 0;
